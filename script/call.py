5b27bb00ebc4449f4c5377b5080e6485ea5802bd
ccortes492 <ccortes492@gmail.com>
2016-01-17 20:19:11 +0100

    Scripting with cores

--- script/call.py
+++ script/call.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
 from subprocess import call
+import time
 
+optimizers = ['em','online']
+cluster = ['local[1]', 'local[4]', 'spark://reed:7077']
+db  = ['100db.txt','050db.txt', '025db.txt', '012db.txt', '006db.txt']
+cores = [1,4,8,16]
+
+for opt in optimizers:
+	for clus in cluster:
+		for data in db:
+			for num in cores: 
+				print "Empiezo "+data+opt+clus
+				command = '/usr/local/spark/bin/spark-submit --class "TestApp" --master '+ clus+' --total-executor-cores '+str(num)+' ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar '+data+' '+opt+
+				return_code = call(command, shell=True)
+				print "Acabo "+data+opt+clus
+				time.sleep(10)
+"""
 return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 100db.txt em', shell=True)
 
 return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 100db.txt online', shell=True)
@@ -35,3 +51,5 @@ return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master
 return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 006db.txt online',shell=True)
 
 print "Acabo 006db"
+
+"""
