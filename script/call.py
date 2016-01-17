e821eb459bed44fd02a32384b2c935d09fd9b2a1
ccortes492 <ccortes492@gmail.com>
2016-01-17 19:42:14 +0100

    call Script v2

--- script/call.py
+++ script/call.py
@@ -17,15 +17,21 @@ return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master
 
 print "Acabo 50db"
 
-return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 025db.txt',shell=True)
+return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 025db.txt em',shell=True)
+
+return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 025db.txt online',shell=True)
 
 print "Acabo 025db"
 
-return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 012db.txt',shell=True)
+return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 012db.txt em',shell=True)
+
+return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 012db.txt online',shell=True)
 
 
 print "Acabo 012db"
 
-return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 006db.txt',shell=True)
+return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 006db.txt em',shell=True)
+
+return_code = call('/usr/local/spark/bin/spark-submit --class "TestApp" --master spark://reed:7077 ../TestApp/target/scala-2.10/testapp_2.10-1.0.jar 006db.txt online',shell=True)
 
 print "Acabo 006db"
